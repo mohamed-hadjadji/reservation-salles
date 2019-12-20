18140457ff7bb0366d049d64ea3eeb118df3597c
anthony-reboli <55872129+anthony-reboli@users.noreply.github.com>
2019-12-20 16:00:55 +0100

    marche apepres

---
+++ planing.php
@@ -0,0 +1,72 @@
+<?php
+session_start();  
+?>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Planing</title>
+	</head>
+		<header>
+		</header>
+			<body>
+
+<h1>planing</h1>
+
+<table>
+	<thead>
+		<th></th>
+		<th>lundi</th>
+		<th>mardi</th>
+		<th>mercredi</th>
+		<th>jeudi</th>
+		<th>vendredi</th>
+	</thead>
+<?php
+$connexion = mysqli_connect("localhost","root","","reservationsalles");
+$requete ="SELECT * FROM reservations";
+$query= mysqli_query($connexion,$requete);
+$resultat= mysqli_fetch_all($query);
+var_dump($resultat);
+
+
+
+// include 'tableau.php';
+
+ for($h=8; $h <=19; $h++){
+ 	echo "<tr>";
+ 	echo"<td>$h</td>";
+
+ 	 	 	for($jour= 1 ; $jour <=5 ; $jour++){
+ 	
+
+ 		  echo "<td>";
+
+
+ 	
+ 					foreach ($resultat as $resultat2) {
+ 						$heure=date("G" , strtotime($resultat2[3]));
+ 						 // echo "$heure<br />";
+ 						// echo "limite";
+ 						$jour2=date("w" , strtotime($resultat2[3]));
+ 						// echo "$jour2<br />";
+
+ 						if($jour==$jour2 && $h==$heure)
+ 						{
+ 							echo "ok event";
+ 						}
+ 						
+ 					}
+ 					echo "</td>";
+ 	}
+ 	echo "</tr>";
+
+}
+
+
+?>
+
+ 	
+
+</html>
